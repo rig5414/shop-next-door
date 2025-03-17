c7de8b21a7b887f05ff182d77031045b61cb0ed8
rig5414 <manassehtelle90@gmail.com>
2025-03-17 17:51:38 +0300

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://shop-next-door.onrender.com', // Replace with your live site URL
+    generateRobotsTxt: true, // Generates a robots.txt file
+    generateIndexSitemap: false, // Set to false to generate individual sitemaps
+  };
+  
